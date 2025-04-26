268bd9f39352be5279c3e6424e60a12742674504
José Alberto Ulloa Avila <ackertaker69@gmail.com>
2025-04-26 01:13:41 -0600

    Agregar proyecto Generar Reportes V2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
-
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ['@fontsource-variable/jetbrains-mono'],
+    },
   },
-
-  integrations: [mdx()]
+  integrations: [mdx()],
 });
